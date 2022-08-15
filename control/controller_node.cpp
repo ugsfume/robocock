9d9efd1152d58aebfc7f3e397f2657759860980f
ugsfume <kenneth.liu.k.h@gmail.com>
2022-08-15 17:46:49 +0800

    created controller package for omni(not working)

---
+++ control/controller_node.cpp
@@ -0,0 +1,12 @@
+#include <controller.h>
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "controller");
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+
+  controller contrl(nh, nh_private);
+  ros::spin();
+}
